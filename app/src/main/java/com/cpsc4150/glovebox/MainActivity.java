088f00d6e655235da9dddffb9f7cd96519b253ff
Shelton Pinson <sheltonpinson@gmail.com>
2019-11-05 00:04:41 -0500

    frags

--- app/src/main/java/com/cpsc4150/glovebox/MainActivity.java
+++ app/src/main/java/com/cpsc4150/glovebox/MainActivity.java
@@ -1,8 +1,11 @@
 package com.cpsc4150.glovebox;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +14,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setOnTabSelectedListener(tabListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HistoryFragment()).commit();
     }
+    private TabLayout.OnTabSelectedListener tabListener =
+            new TabLayout.OnTabSelectedListener() {
+                @Override
+                public void onTabSelected(TabLayout.Tab tab) {
+                    Fragment selectedFragment = null;
+                    switch(tab.getPosition()){
+                        case 0:
+                            selectedFragment = new HistoryFragment();
+                            break;
+                        case 1:
+                            selectedFragment = new NewItemFragment();
+                            break;
+                        case 2:
+                            selectedFragment = new UpcomingFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                }
+
+                @Override
+                public void onTabUnselected(TabLayout.Tab tab) {
+
+                }
+
+                @Override
+                public void onTabReselected(TabLayout.Tab tab) {
+
+                }
+            };
 
 
 }
