8743ebb737405e9b668c2d4e2d51e4aa03c51f92
Shelton Pinson <sheltonpinson@gmail.com>
2019-11-13 09:52:44 -0500

    Added backend code for buttons for new services
    not complete

--- app/src/main/java/com/cpsc4150/glovebox/Adapters/NewItemAdapter.java
+++ app/src/main/java/com/cpsc4150/glovebox/Adapters/NewItemAdapter.java
@@ -1,4 +1,21 @@
 package com.cpsc4150.glovebox.Adapters;
 
-public class NewItemAdapter {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.cpsc4150.glovebox.R;
+
+public class NewItemAdapter extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_newitemfragment);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+//        Fragment fragment = fragmentManager.findFragmentById(R.id.)
+    }
+
 }
--- app/src/main/java/com/cpsc4150/glovebox/Fragments/NewItemFragment.java
+++ app/src/main/java/com/cpsc4150/glovebox/Fragments/NewItemFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,6 +16,32 @@ public class NewItemFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_newitemfragment,container,false);
+        View v =  inflater.inflate(R.layout.fragment_newitemfragment,container,false);
+        Button coolantServiceButton = (Button) v.findViewById(R.id.CoolantServiceButton);
+        Button oilServiceButton = (Button) v.findViewById(R.id.oilChangeButton);
+        Button otherServiceButton = (Button) v.findViewById(R.id.otherServiceButton);
+        Button transServiceButton = (Button) v.findViewById(R.id.otherServiceButton);
+        Button tireRotationButton = (Button) v.findViewById(R.id.tireRotationButton);
+        Button ignitionServiceButton = (Button) v.findViewById(R.id.ignitionButton);
+
+        coolantServiceButton.setOnClickListener(this){
+
+        };
+        oilServiceButton.setOnClickListener(){
+
+        };
+        otherServiceButton.setOnClickListener(){
+
+        };
+        transServiceButton.setOnClickListener() {
+
+        };
+        tireRotationButton.setOnClickListener(){
+
+        };
+        ignitionServiceButton.setOnClickListener(){
+
+        };
+        return(v);
     }
 }
