0e60e3b4bfa2d6fc2eec6c3f59a3949a13036383
Logan Goss <ltgoss@g.clemson.edu>
2019-11-18 16:53:51 -0500

    Added bundles to pass the text of the button pressed to the service fragment. These bundles are a part of each on click listener in NewItemFragment.
    An xml file was changed to add horizontal constraints.
    
    Last small bug where a two button declarations in NewItemFragment were mapped to the same button that has also been fixed.

--- app/src/main/java/com/cpsc4150/glovebox/Fragments/NewItemFragment.java
+++ app/src/main/java/com/cpsc4150/glovebox/Fragments/NewItemFragment.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -21,12 +20,16 @@ public class NewItemFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =  inflater.inflate(R.layout.fragment_newitem,container,
                 false);
-        Button coolantServiceButton = (Button) v.findViewById(R.id.CoolantServiceButton);
-        Button oilServiceButton = (Button) v.findViewById(R.id.oilChangeButton);
-        Button otherServiceButton = (Button) v.findViewById(R.id.otherServiceButton);
-        Button transServiceButton = (Button) v.findViewById(R.id.otherServiceButton);
-        Button tireRotationButton = (Button) v.findViewById(R.id.tireRotationButton);
-        Button ignitionServiceButton = (Button) v.findViewById(R.id.ignitionButton);
+        final Button coolantServiceButton = (Button) v.findViewById(R.id.CoolantServiceButton);
+        final Button oilServiceButton = (Button) v.findViewById(R.id.oilChangeButton);
+        final Button otherServiceButton = (Button) v.findViewById(R.id.otherServiceButton);
+        final Button transServiceButton = (Button) v.findViewById(R.id.transServiceButton);
+        final Button tireRotationButton = (Button) v.findViewById(R.id.tireRotationButton);
+        final Button ignitionServiceButton = (Button) v.findViewById(R.id.ignitionButton);
+
+        //Each on click listener now contains a bundle that passes the title of the button pressed
+        //to the service fragment for better readability. 
+
 
         final FragmentManager fragmentManager = getFragmentManager();
         coolantServiceButton.setOnClickListener(new View.OnClickListener(){
@@ -34,6 +37,9 @@ public class NewItemFragment extends Fragment {
             public void onClick(View v){
                 Log.i("onClick coolantServiceButton", "should open ServiceFragment");
                 Fragment service = new ServiceFragment();
+                Bundle name = new Bundle();
+                name.putString("Name",coolantServiceButton.getText().toString());
+                service.setArguments(name);
                 fragmentManager.beginTransaction().replace(R.id.fragment_container, service).
                         setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit();
             }
@@ -43,6 +49,9 @@ public class NewItemFragment extends Fragment {
            public void onClick(View v) {
                 Log.i("onClick oilServiceButton", "should open ServiceFragment");
                 Fragment service = new ServiceFragment();
+                Bundle name = new Bundle();
+                name.putString("Name",oilServiceButton.getText().toString());
+                service.setArguments(name);
                 fragmentManager.beginTransaction().replace(R.id.fragment_container, service).
                         setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit();
             }
@@ -52,6 +61,9 @@ public class NewItemFragment extends Fragment {
            public void onClick(View v) {
                Log.i("onClick oilServiceButton", "should open ServiceFragment");
                Fragment service = new ServiceFragment();
+               Bundle name = new Bundle();
+               name.putString("Name",otherServiceButton.getText().toString());
+               service.setArguments(name);
                fragmentManager.beginTransaction().replace(R.id.fragment_container, service).
                        setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit();
            }
@@ -61,6 +73,9 @@ public class NewItemFragment extends Fragment {
            public void onClick(View v) {
                Log.i("onClick oilServiceButton", "should open ServiceFragment");
                Fragment service = new ServiceFragment();
+               Bundle name = new Bundle();
+               name.putString("Name",transServiceButton.getText().toString());
+               service.setArguments(name);
                fragmentManager.beginTransaction().replace(R.id.fragment_container, service).
                        setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit();
            }
@@ -70,6 +85,9 @@ public class NewItemFragment extends Fragment {
            public void onClick(View v) {
                Log.i("onClick oilServiceButton", "should open ServiceFragment");
                Fragment service = new ServiceFragment();
+               Bundle name = new Bundle();
+               name.putString("Name",tireRotationButton.getText().toString());
+               service.setArguments(name);
                fragmentManager.beginTransaction().replace(R.id.fragment_container, service).
                        setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit();
            }
@@ -79,6 +97,9 @@ public class NewItemFragment extends Fragment {
            public void onClick(View v) {
                Log.i("onClick oilServiceButton", "should open ServiceFragment");
                Fragment service = new ServiceFragment();
+               Bundle name = new Bundle();
+               name.putString("Name",ignitionServiceButton.getText().toString());
+               service.setArguments(name);
                fragmentManager.beginTransaction().replace(R.id.fragment_container, service).
                        setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit();
            }
