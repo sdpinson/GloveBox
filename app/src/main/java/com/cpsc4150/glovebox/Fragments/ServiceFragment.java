a6288c335072bfa441600c8717324d44b37abdb7
Shelton Pinson <sheltonpinson@gmail.com>
2019-11-18 13:36:33 -0500

    General updates

--- app/src/main/java/com/cpsc4150/glovebox/Fragments/ServiceFragment.java
+++ app/src/main/java/com/cpsc4150/glovebox/Fragments/ServiceFragment.java
@@ -1,9 +1,12 @@
 package com.cpsc4150.glovebox.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,6 +20,15 @@ public class ServiceFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =  inflater.inflate(R.layout.fragment_service,container,
                 false);
+        Button pictureButtonOne = (Button) v.findViewById(R.id.button4);
+        pictureButtonOne.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+                startActivityForResult(intent,0);
+            }
+
+        });
         return(v);
     }
 }
