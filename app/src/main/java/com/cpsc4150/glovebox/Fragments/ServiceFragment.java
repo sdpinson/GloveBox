9abb90195ac23c1b5777d686979fc96d36e9285f
Shelton Pinson <sheltonpinson@gmail.com>
2019-11-24 22:05:55 -0500

    Working Bitmap on NewService

--- app/src/main/java/com/cpsc4150/glovebox/Fragments/ServiceFragment.java
+++ app/src/main/java/com/cpsc4150/glovebox/Fragments/ServiceFragment.java
@@ -30,9 +30,11 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static android.app.Activity.RESULT_OK;
+
 public class ServiceFragment extends Fragment {
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    static final int RESULT_OK = 1;
+    //static final int RESULT_OK = 1;
     private static final int REQUEST_TAKE_PHOTO = 1;
     private Services service;
     String currentPhotoPath;
@@ -81,38 +83,60 @@ public class ServiceFragment extends Fragment {
         return (v);
     }
 
+//    private void dispatchTakePictureIntent() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        // Ensure that there's a camera activity to handle the intent
+//        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+//            // Create the File where the photo should go
+//            File photoFile = null;
+//            try {
+//                photoFile = createImageFile();
+//            } catch (IOException ex) {
+//                Log.e("File error","File null");
+//                // Error occurred while creating the File
+//            }
+//            // Continue only if the File was successfully created
+//            if (photoFile != null) {
+//                Uri photoURI = FileProvider.getUriForFile(getActivity(),
+//                        "com.cpsc4150.glovebox",
+//                        photoFile);
+//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+//                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+//            }
+//        }
+//    }
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(getActivity(),
-                        "com.cpsc4150.glovebox",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-            }
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        //if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            ImageView imageView = (ImageView) getActivity().findViewById(R.id.smallImageView);
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap); // use this to display a thumnail
-            Log.i("image set","image set");
-        //}
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        String debug = " "+requestCode+resultCode+" ";
+//        Log.i("requestCode, resultCode =",debug);
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == -1) {
+//            ImageView imageView = (ImageView) getActivity().findViewById(R.id.smallImageView);
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            imageView.setImageBitmap(imageBitmap); // use this to display a thumnail
+//            Log.i("image set","image set");
+//        }
+//    }
+@Override
+public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(!data.hasExtra("data")){
+            Log.e("Error: ","Bitmap does not exist");
+        }
+    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+        ImageView imageView = (ImageView) getActivity().findViewById(R.id.smallImageView);
+        Bundle extras = data.getExtras();
+        Bitmap imageBitmap = (Bitmap) extras.get("data");
+        Log.e("imageset","imageset");
+        imageView.setImageBitmap(imageBitmap);
     }
+}
 
     private File createImageFile() throws IOException {
         // Create an image file name
